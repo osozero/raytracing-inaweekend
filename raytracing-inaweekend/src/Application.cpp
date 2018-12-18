bd7a739c2006762540b358c551a18513e5f1089b
osozero <sametozer13@gmail.com>
2018-12-17 18:39:20 -0800

    random scene generator implemented.

--- raytracing-inaweekend/src/Application.cpp
+++ raytracing-inaweekend/src/Application.cpp
@@ -8,6 +8,7 @@
 #include "camera.h"
 #include <random>
 #include "material.h"
+#include <corecrt_math_defines.h>
 
 
 vec3 color(const ray& r,hitable *world,int depth)
@@ -33,67 +34,84 @@ vec3 color(const ray& r,hitable *world,int depth)
 	return (1.0 - f)*vec3(1.0f, 1.0f, 1.0f) + f * vec3(0.5f, 0.7f, 1.0f);
 }
 
+hitable *random_scene() {
+	std::default_random_engine gen;
+	std::uniform_real_distribution<float> dist(0.0, 1.0);
+
+	int n = 500;
+	hitable **list = new hitable*[n + 1];
+	list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+	int i = 1;
+	for (int a = -11; a < 11; a++) {
+		for (int b = -11; b < 11; b++) {
+			float choose_mat = dist(gen);
+			vec3 center(a + 0.9*dist(gen), 0.2, b + 0.9*dist(gen));
+			if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+				if (choose_mat < 0.8) {  // diffuse
+					list[i++] = new sphere(center, 0.2, new lambertian(vec3(dist(gen)*dist(gen), dist(gen)*dist(gen), dist(gen)*dist(gen))));
+				}
+				else if (choose_mat < 0.95) { // metal
+					list[i++] = new sphere(center, 0.2,
+						new metal(vec3(0.5*(1 + dist(gen)), 0.5*(1 + dist(gen)), 0.5*(1 + dist(gen))), 0.5*dist(gen)));
+				}
+				else {  // glass
+					list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+				}
+			}
+		}
+	}
+
+	list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+	list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+	list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+	return new hitable_list(list, i);
+}
 
 
 int main()
 {
-	int nx = 200;
-	int ny = 100;
-	int ns = 100;
-
 	std::default_random_engine gen;
-	std::uniform_real_distribution<float> dist(0.0,1.0);
+	std::uniform_real_distribution<float> dist(0.0, 1.0);
 
+	int nx = 1200;
+	int ny = 800;
+	int ns = 10;
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-	
 	hitable *list[5];
-
-	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+	float R = cos(M_PI / 4);
+	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
 	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2),0.3));
+	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.0));
 	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
 	list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
-
 	hitable *world = new hitable_list(list, 5);
+	world = random_scene();
 
-	vec3 lookfrom(3, 3, 2);
-	vec3 lookat(0, 0, -1);
+	vec3 lookfrom(13, 2, 3);
+	vec3 lookat(0, 0, 0);
+	float dist_to_focus = 10.0;
+	float aperture = 0.1;
 
-	float dist_to_focus = (lookfrom - lookat).length();
+	camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(nx) / float(ny), aperture, dist_to_focus);
 
-	float aperture = 2.0;
-
-	camera cam(lookfrom,lookat,vec3(0,1,0),20,float(nx) / float(ny),aperture,dist_to_focus);
-
-	for(int j=ny-1;j>=0;j--)
-	{
-		for(int i=0;i<nx;i++)
-		{
+	for (int j = ny - 1; j >= 0; j--) {
+		for (int i = 0; i < nx; i++) {
 			vec3 col(0, 0, 0);
-
-			for(int s=0;s<ns;s++)
-			{
-				float u = float(i +dist(gen)) / float(nx);
+			for (int s = 0; s < ns; s++) {
+				float u = float(i + dist(gen)) / float(nx);
 				float v = float(j + dist(gen)) / float(ny);
-				
 				ray r = cam.get_ray(u, v);
-
 				vec3 p = r.point_at_parameter(2.0);
-				col += color(r, world,0);
+				col += color(r, world, 0);
 			}
-
 			col /= float(ns);
-			
-			//gamma 2
 			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
-
 			int ir = int(255.99*col[0]);
 			int ig = int(255.99*col[1]);
 			int ib = int(255.99*col[2]);
-
-			std::cout << ir << " " << ig << " " << ib << std::endl;
+			std::cout << ir << " " << ig << " " << ib << "\n";
 		}
 	}
-
 	return 0;
 }
